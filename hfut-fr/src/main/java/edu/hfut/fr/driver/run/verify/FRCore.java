dfdb680150026d2ba30e7c4484a2b3b232413b0a
王刚 <wanggang@zxisl.com>
2015-11-04 16:39:34 +0800

    增加人脸检测生成代码

--- hfut-fr/src/main/java/edu/hfut/fr/driver/run/verify/FRCore.java
+++ hfut-fr/src/main/java/edu/hfut/fr/driver/run/verify/FRCore.java
@@ -1,8 +1,15 @@
 package edu.hfut.fr.driver.run.verify;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.colour.RGBColour;
+import org.openimaj.image.colour.Transforms;
 
 import edu.hfut.fr.driver.run.impl.CosineDissimilarity;
 import edu.hfut.fr.driver.run.impl.EuclideanDistance;
@@ -18,6 +25,8 @@ import edu.hfut.fr.driver.run.impl.Metric;
 import edu.hfut.fr.driver.run.impl.PCA;
 import edu.hfut.fr.driver.run.impl.TrainingMatrix;
 import edu.hfut.fr.driver.run.impl.VerifyData;
+import edu.hfut.fr.image.processing.face.detection.keypoints.FKEFaceDetector;
+import edu.hfut.fr.image.processing.face.detection.keypoints.KEDetectedFace;
 
 /**
  * 人脸识别主类：包括数据加载、特征提取、训练和识别
@@ -69,6 +78,33 @@ public class FRCore {
 		}
 		long endTest = System.currentTimeMillis();
 		System.err.println(testData.getMatrixs().size() + "张图片总测试时间为：" + (endTest - startTest) + " 毫秒");
+	}
+
+	public static void detect(String fileDir) throws IOException {
+		MBFImage colorImage = ImageUtilities.readMBF(new File(fileDir));
+		FKEFaceDetector faceDetector = new FKEFaceDetector(20);
+		List<KEDetectedFace> faces = faceDetector.detectFaces(Transforms.calculateIntensity(colorImage));
+		KEDetectedFace maxFace = null;
+		int max = Integer.MIN_VALUE;
+		// 找出检测中的最大图像块即为人脸
+		for (KEDetectedFace face : faces) {
+			if (face.getFacePatch().getHeight() > max) {
+				max = face.getFacePatch().getHeight();
+				maxFace = face;
+			}
+		}
+		// 在原图上标出人脸部分
+		if (maxFace == null) {
+			System.err.println("检测不到人脸图片");
+		}
+		colorImage.drawShape(maxFace.getBounds(), RGBColour.RED);
+		DisplayUtilities.display(colorImage);
+		// 如果检测到则录入样本库
+		//		AffineAligner faceAligner = new AffineAligner();
+		//		if (maxFace != null) {
+		//			FImage faceFA = faceAligner.align(maxFace);
+		//			ImageUtilities.write(faceFA, "png", new File(""));
+		//		}
 	}
 
 	public void train(String faceDir, int faceType, int faceCount) {
